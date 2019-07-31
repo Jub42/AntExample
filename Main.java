b716b38e65ca7e2e3500df3c23e1bccc75a75de2
MBrill <manfred.brill@hs-kl.de>
2019-07-31 10:09:58 +0200

    Ant-Projekt mit externen jar-files
    Wir verwenden log4j2 als Beispiel für die Verwendung von externen
    Bibliotheken in einem Ant-Projekt.
    
    Wir verändern auch leicht die Verzeichnisstruktur. Die class-Files und
    die Konfiguration für das Logging werden bei compile in ein
    bin-Verzeichnis kopiert. Dort wird bei run auch die Anwendung
    ausgeführt.

--- Main.java
+++ Main.java
@@ -1,20 +1,15 @@
-public class Main {
-	
-    
-	    public static void main(String[] args) {
-        
-	    	One  one = new One();
-	    	Two  two = new Two();
+import org.apache.logging.log4j.LogManager;import org.apache.logging.log4j.Logger;/** * Anwendungsklasse. *  * Wir verwenden die beiden Klassen One und Two, und geben * zusätzlich noch einen weiteren String auf der Konsole aus. *  * @author brill * @version Wintersemester 2019/20 */public class Main {	    /**	     * Instanz eines Loggers für log4j2.	     */        private static final Logger log = LogManager.getLogger();            
+	    public static void main(String[] args) {	    				log.trace(">> Main.main");			
+	    	One  one = new One();
+	    	Two  two = new Two();
 	    	Main three = new Main();
-	    	
-	    	System.out.println(one);
-	    	System.out.println(two);
-	    	System.out.println(three.toString());
+	    	System.out.println(one);
+	    	System.out.println(two);
+	    	System.out.println(three.toString());	    				log.trace("<< Main.main");
 	    }	
-
-
-    	@Override
-        public String toString() {
-        	return "Three!";
-        }
+	    /**	     * Ausgabe auf der Konsole.	     */
+    	@Override
+        public String toString() {			log.trace(">> Main.tostring");    		
+        	return "Three!";
+        }
 }
